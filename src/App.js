9dbfe16e934be92ada6e108fd4292f1064e3521a
khajor <kyji8art@gmail.com>
2020-02-04 10:33:18 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
+  const [text, setText] = useState('')
+  const [memes, setMemes] = useState([])
+  const[loading, showLoading] = useState(false)
+
+  async function getMemes(){
+    showLoading(true)
+    setMemes([])
+    const key = 'ZwJZrSXXlsqnZZhBNhfKOWRMn9OhjGF8' //Api key!
+    let url = 'https://api.giphy.com/v1/gifs/search?' //base url 
+    url += 'api_key=' + key //tack on the api key to the url
+    url += '&q=' + text //tack on the search words to the url
+    url += '&limit=1000'
+    const resp = await fetch(url) //fetch is a built in function for the browser 
+    const body = await resp.json() //get the body out of the response 
+    setMemes(body.data) 
+    setText('')
+    showLoading(false)
+  } 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="input-wrap">
+          <TextField fullWidth variant="outlined" color='secondary' autoFocus='true'
+            label="Search for memes" 
+            value={text}
+            onChange={e=> setText(e.target.value)}
+            className='text-field'
+            onKeyPress={e=>{
+              if(e.key==='Enter') getMemes()
+            }}
+          />
+          <Button variant="contained" color="secondary"
+          onClick={getMemes}> {/*never put parentheses at the end of the function unless theres a fat arrow before it*/}
+            Search
+          </Button>
+        </div>
       </header>
+
+      <div className="meme-wrapper">
+        <div className='loading-wrap'>{loading && <CircularProgress color='secondary' className='loading-circle'/>}</div>
+        {memes.map((meme,i)=> <Meme key={i} {...meme} />)}
+      </div>
+
     </div>
   );
 }
+
+function Meme({title, images}){
+  return <div className="meme">
+    <img src={images.fixed_height.url} alt="memes"/>
+    <div className="meme-title">{title}</div>
+  </div>
+} 
 
 export default App;
