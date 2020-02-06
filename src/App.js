24ad235100c4605b8a60d0a9d2fa82c979313030
khajor <kyji8art@gmail.com>
2020-02-05 21:04:37 -0800

    added external link to images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import {Search} from '@material-ui/icons';
 
 function App() {
   const [text, setText] = useState('')
@@ -17,9 +17,10 @@ function App() {
     let url = 'https://api.giphy.com/v1/gifs/search?' //base url 
     url += 'api_key=' + key //tack on the api key to the url
     url += '&q=' + text //tack on the search words to the url
-    url += '&limit=1000'
+    url += '&limit=100' 
     const resp = await fetch(url) //fetch is a built in function for the browser 
     const body = await resp.json() //get the body out of the response 
+    console.log(body.data)
     setMemes(body.data) 
     setText('')
     showLoading(false)
@@ -40,25 +41,27 @@ function App() {
           />
           <Button variant="contained" color="secondary"
           onClick={getMemes}> {/*never put parentheses at the end of the function unless theres a fat arrow before it*/}
-            Search
+            <Search/>
           </Button>
         </div>
       </header>
 
       <div className="meme-wrapper">
         <div className='loading-wrap'>{loading && <CircularProgress color='secondary' className='loading-circle'/>}</div>
-        {memes.map((meme,i)=> <Meme key={i} {...meme} />)}
+        {memes.map((meme, i)=> <Meme key={i} {...meme} />)} {/*...meme means all of the memes in that array */}
       </div>
 
     </div>
   );
 }
 
-function Meme({title, images}){
-  return <div className="meme">
-    <img src={images.fixed_height.url} alt="memes"/>
-    <div className="meme-title">{title}</div>
-  </div>
+function Meme({images, title}){
+  const url = images.fixed_height.url
+  return (
+    <div className="meme" onClick={()=>window.open(url, '_blank')}>
+      <img src={images.fixed_height.url} alt="memes"/>
+      <div className="meme-title">{title}</div>
+    </div>)
 } 
 
 export default App;
